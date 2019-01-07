caa1ffa824e256588186489ba143bfa936656f8c
James <james@derivatived.com>
2019-01-07 18:03:31 -0500

    reset

---
+++ nodejs/AdvancedNodeStarter2/config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://james:what4444@ds129454.mlab.com:29454/blog_dev',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AWS',
+  secretAccessKey: 'AWS'
+};
